2ad7ec701ae029883a7c5f2a62bd9a38b8008f8e
hector <haitao.qin@dsmake.com>
2019-10-26 11:47:01 +0800

    Modify file

--- cron.php
+++ cron.php
@@ -12,7 +12,7 @@ function get_include_contents($filename) {
     return false;
 }
 
-$mailContent = get_include_contents('views/get_top100_slowsql.php');
+$mailContent = get_include_contents('views/top100_slowquery.php');
 
 $title='【告警】慢查询报警推送TOP100条,请及时优化.';
 $content='下面的慢查询语句或许会影响到数据库的稳定性和健康性，请您在收到此邮件后及时优化语句或代码。数据库的稳定性需要大家的共同努力,感谢您的配合！<br><br>' .$mailContent;
